c3a9952c7d7e439244baabb5140eaf6de42f02a7
CosinusXTR <alper53m@gmail.com>
2024-05-26 12:28:34 +0300

    Connection problems solved
    Connection problems solved

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import connectMongo from './db/connectMongo.js';
 import sequelize from './db/sequelize.js';
-import authRoutes from './routes/auth.routes.js';
-import passengerRoutes from './routes/passenger.routes.js';
-import flightRoutes from './routes/flight.routes.js';
+
 import pilotRoutes from './routes/pilot.routes.js';
 
 dotenv.config();
@@ -21,9 +19,7 @@ sequelize.authenticate()
   .catch(err => console.log('Sequelize connection error:', err));
 
 // Routes
-app.use('/api/auth', authRoutes); // Use the auth routes
-app.use('/api/passengers', passengerRoutes); // Use the passenger routes
-app.use('/api/flights', flightRoutes);
+
 app.use('/api/pilots', pilotRoutes);
 
 const port = process.env.PORT || 3000;
